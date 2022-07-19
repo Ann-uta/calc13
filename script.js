07123722cf76ff5b26c74924164a2e7d17f165bc
Anna <momannuta@gmail.com>
2022-07-19 14:22:03 +0300

    add theme color

--- script.js
+++ script.js
@@ -12,28 +12,51 @@ let result;
 function showResult() {
     div.textContent = `Результат: ${result}`;
 }
+function clear(){
+    document.getElementById('errorMessage').innerHTML="";
+}
 
 function sum() {
+    clear()
     result = Number(input1.value)+Number(input2.value);
     showResult ();
 }
 
 function sub() {
+    clear()
     result = Number(input1.value)-Number(input2.value);
     showResult ();
 }
 function mult() {
+    clear()
     result = Number(input1.value)*Number(input2.value);
     showResult ();
 }
+
 function division() {
-    result = Number(input1.value)/Number(input2.value);
-    showResult ();
+    clear()
+        if (input2.value == 0){
+            document.getElementById('errorMessage').innerHTML = "На 0 делить нельзя!";
+    } 
+        result = Number(input1.value)/Number(input2.value);
+        showResult ();             
 }
+
 
 // функция для изменения фона div по клику //
 
 function ChangeBgC() {
     let div = document.getElementsByClassName("color");
     div[0].style.background = "blue";
+}
+
+// функции для изменения Темы //
+
+function changeTheme() {    
+    if (document.getElementById("theme").value == "default")
+    document.body.style.background = "white";
+    if (document.getElementById("theme").value == "dark")
+    document.body.style.background = "#2f4f4f";
+    if (document.getElementById("theme").value == "light")
+    document.body.style.background = "#faf0e6";
 }
